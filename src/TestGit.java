ae59799bc89714b37804ba5a1a2107bb77e9f81c
liuning <test@runoob.com>
2018-02-09 11:33:13 +0800

    第一次修改

--- src/TestGit.java
+++ src/TestGit.java
@@ -3,6 +3,6 @@
  */
 public class TestGit {
     public static void main(String[] args) {
-        System.out.println("my name is liuning");
+        System.out.println("my name is liuning yuan");
     }
 }
